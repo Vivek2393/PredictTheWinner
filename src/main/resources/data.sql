72f9a79d1dd10368e38d5e9d12e7d2f7535d0eda
Vivek2393 <blogupvivek@gmail.com>
2023-08-17 21:05:05 +0530

    data.sql file added

---
+++ src/main/resources/data.sql
@@ -0,0 +1,17 @@
+Insert into Team VALUES (101, 'ENG')
+Insert into Team VALUES (202, 'WI')
+Insert into Team VALUES (303, 'IND')
+Insert into Team VALUES (404, 'PAK')
+Insert into Team VALUES (505, 'AUS')
+Insert into Team VALUES (606, 'NZ')
+Insert into Team VALUES (707, 'BAN')
+Insert into Team VALUES (808, 'AFG')
+Insert into Team VALUES (909, 'SL')
+Insert into Team VALUES (1010, 'SCO')
+
+Insert into Matches VALUES (1, 505, 606, NULL)
+Insert into Matches VALUES (2, 202, 707, NULL)
+Insert into Matches VALUES (3, 404, 303, NULL)
+Insert into Matches VALUES (4, 707, 808, NULL)
+Insert into Matches VALUES (5, 909, 1010, NULL)
+
